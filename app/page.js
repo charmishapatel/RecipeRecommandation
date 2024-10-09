a75e7b555d45d786c9b1c71fd38b70f5cde1d2fc
charmishapatel <152562360+charmishapatel@users.noreply.github.com>
2024-10-08 23:05:21 -0600

    week4

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
 export default function page(){
   return(
-    <main className="bg-neutral-200 " >
+    <main className="bg-neutral-200 h-screen" >
       <h1 className="text-2xl py-3 text-lime-700" >CPRG 306: Web Development 2 - Assignments </h1>
       <ul>
         <li><Link href="./week-2" className="text-violet-700 pl-2">Week-2 assignments</Link></li>
         <li><Link href="./week-3" className="text-violet-700 pl-2">Week-3 assignments</Link></li>
+        <li><Link href="./week-4" className="text-violet-700 pl-2">Week-4 assignments</Link></li>
+        <li><Link href="./week-3" className="text-violet-700 pl-2">Week-5 assignments</Link></li>
+        <li><Link href="./week-3" className="text-violet-700 pl-2">Week-6 assignments</Link></li>
       </ul>
     </main>
   );
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import ItemList from "./item-list";
 
 export default function Page(){
     return(
-        <main>
+        <main className="bg-slate-950">
             <p className="bg-slate-950  mt-2 text-white text-3xl">Shopping List</p>
             <ItemList/>
         </main>
